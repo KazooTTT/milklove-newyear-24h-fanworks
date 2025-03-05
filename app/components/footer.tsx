cf24fbf68483ec1c9114274a2d88d025d11dca2f
KazooTTT <turbopm@outlook.com>
2025-03-05 17:29:54 +0800

    feat: 在 Footer 中添加 GitHub 链接
    - 在 Footer 中添加 GitHub 链接，指向 milklove-newyear-24h-fanworks 仓库
    - 添加 GitHub 图标，并设置链接样式

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,18 @@ export function Footer() {
     <footer className="w-full py-4 mt-auto bg-gray-50">
       <div className="container mx-auto px-4">
         <div className="text-center text-sm text-gray-600">
-          <p className="mb-2">本站仅做内容链接汇总，感谢每位厨子的产出～</p>
+          <p className="mb-2">
+            本站仅做内容链接汇总，感谢每位老师的产出～
+            <a
+              href={"https://github.com/KazooTTT/milklove-newyear-24h-fanworks"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-2 transform hover:scale-110 transition-transform hover:text-black"
+              aria-label={`Github`}
+            >
+              <i className="fab fa-github text-lg"></i>
+            </a>
+          </p>
         </div>
       </div>
     </footer>
